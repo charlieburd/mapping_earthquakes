a1aaa3e4a47e4cbee74468cb33ddd4e8fb50da61
Charlie Burd <cburd@corp.better.site>
2021-01-09 23:00:24 -0800

    13.3.1

--- simple_map/static/js/logic.js
+++ simple_map/static/js/logic.js
@@ -1,16 +1,12 @@
 // Add console.log to check to see if our code is working
 console.log("working");
 
-
-
 // Create the map object with a center and zoom level.
 //setView is when the page is opened these are the starting coordinates
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 
-
-
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}.png?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     //class key does not have this 'id' line
